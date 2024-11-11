9c908c299462657dcc6ca0f8e8ce5eda6668e557
gerald525 <gerald.lloyd.us@gmail.com>
2024-11-11 02:42:09 +0100

    feat: create home page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import HomeImg from "../../public/2.jpg";
+
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div>
-      <Image src="/2.jpg" width={100} height={100} alt="Filip Home" /> 
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.item}>
+				<h1 className={styles.title}>
+					Better design for your digital products.
+				</h1>
+				<p className={styles.desc}>
+					Turning your idea into reality. We bring together into
+					global tech industry.
+				</p>
+				<button className={styles.button}>See our work</button>
+			</div>
+			<div className={styles.item}>
+				<Image src={HomeImg} alt="Filip Home" className={styles.img} />
+			</div>
+		</div>
+	);
 }
